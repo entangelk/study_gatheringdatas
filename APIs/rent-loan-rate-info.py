bc15140a578c176d9b16316869fe9880eb509c85
entangelk <kdtyohan@gmail.com>
2024-01-19 13:42:34 +0900

    update

--- APIs/rent-loan-rate-info.py
+++ APIs/rent-loan-rate-info.py
@@ -1,9 +1,12 @@
 import requests
 
+# 한국주택금융공사_전세자금대출 금리 정보
+# form :  https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15082033
+
 url='http://apis.data.go.kr/B551408/rent-loan-rate-info/rate-list'
 
 params ={
-    'serviceKey':'U7qOOR4KMrkRvFHfQjd8XQh1DJraaYLetyiqIfNiJEsrwG%2BHRWhPpfVffZDjB0aJtFc9eSmc6tR1iWQat2Stew%3D%3D',
+    'serviceKey':'U7qOOR4KMrkRvFHfQjd8XQh1DJraaYLetyiqIfNiJEsrwG+HRWhPpfVffZDjB0aJtFc9eSmc6tR1iWQat2Stew==',
 'pageNo': 1,
 'numOfRows': 10,
 'dataType': 'JSON'
@@ -12,4 +15,32 @@ params ={
 
 response = requests.get(url,params=params)
 
-print(response.content)
+
+import json
+contents = json.loads(response.content)
+
+type(contents)
+# <class 'dict'>
+contents['header']
+# {"resultCode": "00", "resultMsg": "정상"}
+contents['header']['resultCode']
+# '00'
+contents['body']['totalCount']
+# 18
+type(contents['body']['items'])
+# <class 'list'>
+
+
+# mongodb 저장
+
+from pymongo import MongoClient
+# mongodb에 접속 -> 자원에 대한 class
+mongoClient = MongoClient("mongodb://localhost:27017")
+# database 연결
+database = mongoClient["data_go_kr"]
+# collection 작업
+collection = database['rent-loan-rate-info']
+# insert 작업 진행
+result = collection.insert_many(contents['body']['items'])
+
+pass
